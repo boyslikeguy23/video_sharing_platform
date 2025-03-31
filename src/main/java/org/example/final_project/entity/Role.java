d814ddcafe699bc441fb9c4d46511659d36bbac0
boyslikeguy23 <tungxuanmai2003@gmail.com>
2025-03-31 23:11:24 +0700

    add Role entity

---
+++ src/main/java/org/example/final_project/entity/Role.java
@@ -0,0 +1,24 @@
+package org.example.final_project.entity;
+
+import jakarta.persistence.*;
+import lombok.*;
+
+@Entity
+@Table(name = "role")
+@AllArgsConstructor
+@NoArgsConstructor
+@Getter
+@Setter
+@Builder
+public class Role {
+    @Id
+    @GeneratedValue(strategy = GenerationType.IDENTITY)
+    private Long id;
+
+    @Column(name = "name", nullable = false)
+    private String name;
+
+    public static String ADMIN = "ADMIN";
+    public static String USER = "USER";
+   // public static String ORGANIZER = "ORGANIZER";
+}
