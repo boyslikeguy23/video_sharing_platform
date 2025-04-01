3cf43ae3c8f901b680cc49b9e38dad8b1c29aa5f
boyslikeguy23 <tungxuanmai2003@gmail.com>
2025-04-01 16:27:14 +0700

    add Channel and Subcription entity

---
+++ src/main/java/org/example/final_project/entity/Channel.java
@@ -0,0 +1,29 @@
+package org.example.final_project.entity;
+
+import jakarta.persistence.*;
+import lombok.AllArgsConstructor;
+import lombok.Builder;
+import lombok.Data;
+import lombok.NoArgsConstructor;
+
+@Entity
+@Data
+@Builder
+@AllArgsConstructor
+@NoArgsConstructor
+@Table(name = "channel")
+public class Channel {
+    @Id
+    @GeneratedValue(strategy = GenerationType.IDENTITY)
+    private Long id;
+
+    @OneToOne
+    @JoinColumn(name = "user_id", nullable = false)
+    private User user;
+
+    @Column(name = "name", unique = true, nullable = false, length = 100)
+    private String name;
+
+    @Column(name = "description", columnDefinition = "TEXT")
+    private String description;
+}
