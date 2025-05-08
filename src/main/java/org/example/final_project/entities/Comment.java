c6909ef9258db12674fff4f5070f837737a2cb6e
boyslikeguy23 <tungxuanmai2003@gmail.com>
2025-05-08 20:08:58 +0700

    feat: add LocalDateTime for comment

--- src/main/java/org/example/final_project/entities/Comment.java
+++ src/main/java/org/example/final_project/entities/Comment.java
@@ -1,31 +1,41 @@
 package org.example.final_project.entities;
 
 import jakarta.persistence.*;
+import jakarta.validation.constraints.NotBlank;
 import jakarta.validation.constraints.NotNull;
 import lombok.*;
+
+import java.time.LocalDateTime;
 
 @Entity
 @Table(name = "comment")
 @Getter
 @Setter
-@Builder
 @NoArgsConstructor
 @AllArgsConstructor
+@Builder
 public class Comment extends BaseEntity {
 
     @Id
     @GeneratedValue(strategy = GenerationType.IDENTITY)
-    private Long commentId;
+    private Long id;
 
-    @NotNull
-    @Column(nullable = false)
+    @NotBlank(message = "Content cannot be blank")
+    @Column(nullable = false, columnDefinition = "TEXT")
     private String content;
 
-    @ManyToOne
+    @ManyToOne(fetch = FetchType.LAZY)
     @JoinColumn(name = "video_id", nullable = false)
     private Video video;
 
-    @ManyToOne
+    @ManyToOne(fetch = FetchType.LAZY)
     @JoinColumn(name = "user_id", nullable = false)
     private User user;
+
+    // Optional: Để hỗ trợ bình luận cha - con (nested comments)
+    @ManyToOne(fetch = FetchType.LAZY)
+    @JoinColumn(name = "parent_comment_id")
+    private Comment parentComment;
+
+    private LocalDateTime commentedAt = LocalDateTime.now();
 }
