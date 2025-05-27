55cb8b89654b5fcea632450d95040e94e887c383
boyslikeguy23 <tungxuanmai2003@gmail.com>
2025-05-27 22:14:44 +0700

    feat: add Message entity

---
+++ src/main/java/org/example/final_project/models/Message.java
@@ -0,0 +1,37 @@
+package org.example.final_project.models;
+import jakarta.persistence.*;
+import lombok.Getter;
+import lombok.Setter;
+
+import java.time.LocalDateTime;
+
+@Setter
+@Getter
+@Entity
+@Table(name = "messages")
+public class Message {
+    // Getters and Setters
+    @Id
+    @GeneratedValue(strategy = GenerationType.IDENTITY)
+    private Integer id;
+
+    @ManyToOne(fetch = FetchType.LAZY)
+    @JoinColumn(name = "sender_id", nullable = false)
+    private User sender;
+
+    @ManyToOne(fetch = FetchType.LAZY)
+    @JoinColumn(name = "receiver_id", nullable = false)
+    private User receiver;
+
+    @Column(nullable = false)
+    private String content;
+
+    @Column(nullable = false)
+    private LocalDateTime sentAt;
+
+    private boolean read = false;
+
+    // Constructors
+    public Message() {}
+
+}
