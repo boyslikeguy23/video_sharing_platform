62defb7ab8f0e3bb3d497d528678eb85508c8b93
boyslikeguy23 <tungxuanmai2003@gmail.com>
2025-05-27 22:14:53 +0700

    feat: add Message repository

---
+++ src/main/java/org/example/final_project/repositories/MessageRepository.java
@@ -0,0 +1,30 @@
+package org.example.final_project.repositories;
+
+import org.example.final_project.models.Message;
+import org.example.final_project.models.User;
+import org.springframework.data.jpa.repository.JpaRepository;
+import org.springframework.data.jpa.repository.Query;
+import org.springframework.data.repository.query.Param;
+
+import java.util.List;
+
+public interface MessageRepository extends JpaRepository<Message, Integer>
+{
+    @Query("SELECT m FROM Message m WHERE (m.sender.id = :userId1 AND m.receiver.id = :userId2) " +
+            "OR (m.sender.id = :userId2 AND m.receiver.id = :userId1) ORDER BY m.sentAt")
+    List<Message> findConversation(@Param("userId1") Integer userId1, @Param("userId2") Integer userId2);
+
+    @Query("SELECT DISTINCT CASE " +
+            "WHEN m.sender.id = :userId THEN m.receiver " +
+            "ELSE m.sender END " +
+            "FROM Message m " +
+            "WHERE m.sender.id = :userId OR m.receiver.id = :userId " +
+            "ORDER BY m.sentAt DESC")
+    List<User> findRecentChatUsers(@Param("userId") Integer userId);
+
+    @Query("SELECT m FROM Message m WHERE m.receiver.id = :userId AND m.read = false ORDER BY m.sentAt")
+    List<Message> findUnreadMessages(@Param("userId") Integer userId);
+
+
+
+}
